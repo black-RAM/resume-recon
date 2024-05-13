384a0f58219353d9359960c41e1a4982a832f33a
black-RAM <pplanes311@gmail.com>
2024-05-13 21:10:55 +0300

    page-switching in preview

---
+++ src/components/A4.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from "react"
+
+interface Props {
+  maxWidth: number, 
+  maxHeight: number,
+  overflowAlert: React.Dispatch<React.SetStateAction<boolean>>,
+  children?: React.ReactNode
+}
+
+const A4: React.FC<Props> = ({maxWidth, maxHeight, overflowAlert, children}) => {
+  const pageRef = useRef<HTMLDivElement | null>(null)
+  const portrait = maxWidth * Math.sqrt(2)
+  const landscape = maxHeight / Math.sqrt(2)
+  const width = Math.min(landscape, maxWidth)
+  const height = Math.min(portrait, maxHeight)
+
+  useEffect(() => {
+    const page = pageRef.current
+    if(page) overflowAlert(page.clientHeight < page.scrollHeight)
+  }, [])
+
+  return (
+    <div ref={pageRef} style={{width: width, height: height, fontSize: width / 20}} className="overflow-hidden">
+      {children}
+    </div>
+  )
+}
+
+export default A4
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,42 +1,68 @@
-import React from "react"
-import { DataForm, FormField } from "../interfaces/DataForm"
+import React, { useState } from "react"
+import A4 from "./A4"
+import { DataForm, FormArray, FormField } from "../interfaces/DataForm"
+import useViewport from "../hooks/useViewport"
 import isFormArray from "../utils/isFormArray"
 import hyphenate from "../utils/hyphenate"
-import useViewport from "../hooks/useViewport"
 import "../styles/Preview.css"
+// @ts-ignore
+import chevronLeft from "../assets/chevronLeft.svg"
+// @ts-ignore
+import chevronRight from "../assets/chevronRight.svg"
 
-const Content: React.FC<{obj: FormField}> = ({obj}) => {
+const Paragraphs: React.FC<{obj: FormField}> = ({obj}) => {
   return Object.entries(obj).map(([key, value], index) => 
     <p key={index} className={hyphenate(key)}>{value}</p>
   )
 }
 
-const Preview: React.FC<{data: DataForm}> = ({data}) => {
-  // for a A4 paper aspect ratio
-  const {vw, vh} = useViewport()
-  const maxWidth = 45 * vw
-  const maxHeight = 100 * vh - 92
-  const portrait = maxWidth * Math.sqrt(2)
-  const landscape = maxHeight / Math.sqrt(2)
-  const width = Math.min(landscape, maxWidth)
-  const height = Math.min(portrait, maxHeight)
+const Content: React.FC<{sketch: FormField | FormArray}> = ({sketch}) => {
+  if(isFormArray(sketch)) {
+    return Object.entries(sketch["data"]).map(([id, fields], index) => 
+      <div className="listed" key={index}>
+        <Paragraphs obj={fields} />
+      </div>
+    )
+  } else {
+    return <Paragraphs obj={sketch} />
+  }
+}
 
-  return (
-    <div id="preview" style={{width: width, height: height, fontSize: height / 30}}>{
-      Object.entries(data).map(([sectionName, sectionData], index) => {
-        return <article id={hyphenate(sectionName)} className="article" key={index}>
-          <h3 className="title">{sectionName}</h3>
-          {isFormArray(sectionData) ? 
-            Object.entries(sectionData["data"]).map(([_, fields], index) => {
-              return <div key={index} className="listed">
-                <Content obj={fields} />
-              </div>
-            }) :
-            <Content obj={sectionData} />}
-        </article>
-      })
-    }</div>
+const Preview: React.FC<{data: DataForm}> = ({data}) => {
+  const [isOverflowing, setIsOverflowing] = useState(false)
+  const [pageNumber, flipPage] = useState(0)
+  const {vw, vh} = useViewport()
+
+  const articles = Object.entries(data).map(([sectionName, sectionData], index) => 
+    <article id={hyphenate(sectionName)} className="article" key={index}>
+      <h3 className="title">{sectionName}</h3>
+      <Content sketch={sectionData} />
+    </article>
   )
+
+  const pages = [
+    [articles[0], articles[1]],
+    [articles[2]]
+  ]
+
+  const nextPage = () => flipPage(current => Math.min(current + 1, articles.length))
+  const lastPage = () => flipPage(current => Math.max(current - 1, 0))
+
+  return <>
+    <div className="flex">
+      <button type="button" title="previous page" onClick={lastPage}>
+        <img src={chevronLeft} alt="left arrow" />
+      </button>
+      <A4 maxHeight={100 * vh - 92} maxWidth={45 * vw} overflowAlert={setIsOverflowing}>
+        <div id="preview">
+          {pages[pageNumber]}
+        </div>
+      </A4>
+      <button type="button" title="next page" onClick={nextPage}>
+        <img src={chevronRight} alt="right arrow" />
+      </button>
+    </div>
+  </>
 }
 
 export default Preview
